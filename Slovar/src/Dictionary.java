7a3e9a1b97f6324b64d0c0072e66aed36e6ddfdc
Gevorg <gevorg_rabota@mail.ru>
2023-04-01 22:01:55 +0300

    Result7

--- Slovar/src/Dictionary.java
+++ Slovar/src/Dictionary.java
@@ -4,77 +4,87 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Dictionary implements InterfaceDict{
+public class Dictionary implements InterfaceDict {
 
-        /*для 1 словоря где ключ это цифры*/
-        Map<String, String> map_number = new HashMap<>();
+    /*для 1 словоря где ключ это цифры*/
+    Map<String, String> map_number = new HashMap<>();
 
-        /*для 2 словоря где ключ это буквы*/
-        Map<String, String> map_word = new HashMap<>();
-        String line;
-        String strArray[];
+    /*для 2 словоря где ключ это буквы*/
+    Map<String, String> map_word = new HashMap<>();
+    String line;
+    String strArray[];
 
-        /*Слово которое мы ищем*/
-        String WordSearch;
+    /*Слово которое мы ищем*/
+    String WordSearch;
 
-        public void Read() throws FileNotFoundException {
-            String path = "test.txt";
-            File file = new File(path);
-            Scanner scanner = new Scanner(file);
-            /*проходим по всем строкам, возврощает истину если ещё есть строка*/
-            while (scanner.hasNextLine())
-            {
-                line = scanner.nextLine();//берём эту строку
-                strArray = line.split(" ");
-                map_number.put(strArray[0], strArray[1]);//1 словарь
-                map_word.put(strArray[1], strArray[0]);//2 словарь
-            }
-            scanner.close();//останавливаем поток
+    public void Read() throws FileNotFoundException {
+        String path = "test.txt";
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        /*проходим по всем строкам, возврощает истину если ещё есть строка*/
+        while (scanner.hasNextLine()) {
+            line = scanner.nextLine();//берём эту строку
+            strArray = line.split(" ");
+            map_number.put(strArray[0], strArray[1]);//1 словарь
+            map_word.put(strArray[1], strArray[0]);//2 словарь
         }
-        /*вывести*/
-        public void Conclusion()
-        {
-            if (Main.f == false) {
-                System.out.println((map_number));
-            } else {
-                System.out.println((map_word));
-            }
+        scanner.close();//останавливаем поток
+    }
+
+    /*вывести*/
+    public void Conclusion() {
+        if (Main.f == false) {
+            System.out.println((map_number));
+        } else {
+            System.out.println((map_word));
         }
-        /*удалить*/
-        public void Removal()
-        {
-            if (Main.f == false) {
-                map_number.remove(Main.key);
-            } else {
-                map_word.remove(Main.key);
-            }
+    }
+
+    /*удалить*/
+    public void Removal() {
+        if (Main.f == false) {
+            map_number.remove(Main.key);
+        } else {
+            map_word.remove(Main.key);
         }
-        /*добавить*/
-        public void AddNewWord()
-        {
-            if (Main.f == false) {
-                map_number.put(Main.wordNumbers, Main.wordLetters);
-            } else map_word.put(Main.wordLetters, Main.wordNumbers);
+    }
+
+    /*добавить*/
+    public void AddNewWord() {
+        if (Main.f == false) {
+            map_number.put(Main.wordNumbers, Main.wordLetters);
+        } else map_word.put(Main.wordLetters, Main.wordNumbers);
+    }
+
+    /*Проверка*/
+    public void ChecktheWord() {
+        Boolean f1 = Main.wordNumbers.matches("\\d{5}");
+        Boolean f2 = Main.wordLetters.matches("[a-zA-Z]{4}");
+        if (!f1 && Main.f==false) {
+            System.out.println("Ключ должен состоять из 5 цифр!");
         }
-        /*Проверка*/
-        public void ChecktheWord() {
-            Boolean f1 = Main.wordNumbers.matches("\\d{5}");
-            Boolean f2 = Main.wordLetters.matches("[a-zA-Z]{4}");
-            if (f1 && f2) {
-                AddNewWord();
-            } else {
-                System.out.println("Слово не соответствует указанным требованиям");
-            }
+        if (!f2 && Main.f==false) {
+            System.out.println("Значение должно состоять из 4 латинских букв!");
         }
-        /*поиск*/
-        public void Search()
-        {
-            if (Main.f == false) {
-                WordSearch = map_number.get(Main.keyWords);
-                System.out.println(WordSearch);
-            } else {
-                WordSearch = map_word.get(Main.keyWords);
-                System.out.println(WordSearch);
-            }
+        if (!f1 && Main.f==true) {
+            System.out.println("Значение должно состоять из 5 цифр!");
         }
+        if (!f2 && Main.f==true) {
+            System.out.println("Ключ должен состоять из 4 латинских букв!");
+        }
+        if(f1 && f2){
+            AddNewWord();
+        }
+    }
+
+    /*поиск*/
+    public void Search() {
+        if (Main.f == false) {
+            WordSearch = map_number.get(Main.keyWords);
+            System.out.println(WordSearch);
+        } else {
+            WordSearch = map_word.get(Main.keyWords);
+            System.out.println(WordSearch);
+        }
+    }
 }
--- Slovar/src/Main.java
+++ Slovar/src/Main.java
@@ -24,75 +24,72 @@ public class Main {
         dictionary.Read();
         Scanner in = new Scanner(System.in);
         Scanner sr = new Scanner(System.in);
-        try {
-            while (true) {
-                int choice = scanner.nextInt();
-                switch (choice) {
+        while (true) {
+            String choice = scanner.next();
+            switch (choice) {
+                case "1":
+                    f = false;
+                    System.out.println("Вы открыли 1 словарь, выберите команду из указаного списка");
+                    dictionary.Conclusion();
+                    menu1();
+                    break;
+                case "11":
+                    System.out.println("Введите ключ слова которое вы хотите удалить из 1 словаря");
+                    key = in.next();
+                    dictionary.Removal();
+                    dictionary.Conclusion();
+                    menu1();
+                    break;
+                case "12":
+                    System.out.println("Введите ключ для слова которое вы хотите добавить в 1 словарь (ключ - пятизначное число)");
+                    wordNumbers = sr.next();
 
-                    case 1:
-                        f = false;
-                        System.out.println("Вы открыли 1 словарь, выберите команду из указаного списка");
-                        dictionary.Conclusion();
-                        menu1();
-                        break;
-                        case 11:
-                            System.out.println("Введите ключ слова которое вы хотите удалить из 1 словаря");
-                            key = in.next();
-                            dictionary.Removal();
-                            dictionary.Conclusion();
-                            menu1();
-                            break;
-                        case 12:
-                            System.out.println("Введите ключ для слова которое вы хотите добавить в 1 словарь (ключ - пятизначное число)");
-                            wordNumbers = sr.next();
-                            System.out.println("Введите само слово (значение - слово из 4 латинских букв)");
-                            wordLetters = sr.next();
-                            dictionary.ChecktheWord();
-                            dictionary.Conclusion();
-                            menu1();
-                            break;
-                        case 13:
-                            System.out.println("Введите ключ слова которое вы хотите найти во 1 словаре");
-                            keyWords = scanner.next();
-                            dictionary.Search();
-                            menu1();
-                            break;
-                    case 2:
-                        f = true;
-                        System.out.println("Вы открыли 2 словарь, выберите команду из указаного списка");
-                        dictionary.Conclusion();
-                        menu2();
-                        break;
-                        case 21:
-                            System.out.println("Введите ключ слова которое вы хотите удалить из 2 словаря");
-                            key = in.next();
-                            dictionary.Removal();
-                            dictionary.Conclusion();
-                            menu2();
-                            break;
-                        case 22:
-                            System.out.println("Введите ключ слова которое вы хотите добавить во 2 словарь (Ключ - слово из 4 латинских букв)");
-                            wordLetters = sr.next();
-                            System.out.println("Введите само слово (значение - пятизначное число)");
-                            wordNumbers = sr.next();
-                            dictionary.ChecktheWord();
-                            dictionary.Conclusion();
-                            menu2();
-                            break;
-                        case 23:
-                            System.out.println("Введите ключ слова которое вы хотите найти во 2 словаре.");
-                            keyWords = scanner.next();
-                            dictionary.Search();
-                            menu2();
-                            break;
-                    default:
-                        System.out.println("Выбрана неверная команда. Будьте внимательней!");
-                        System.out.println("1 - открыть 1 словарь");
-                        System.out.println("2 - открыть 2 словарь");
-                }
+                    System.out.println("Введите само слово (значение - слово из 4 латинских букв)");
+                    wordLetters = sr.next();
+
+                    dictionary.ChecktheWord();
+                    dictionary.Conclusion();
+                    menu1();
+                    break;
+                case "13":
+                    System.out.println("Введите ключ слова которое вы хотите найти во 1 словаре");
+                    keyWords = scanner.next();
+                    dictionary.Search();
+                    menu1();
+                    break;
+                case "2":
+                    f = true;
+                    System.out.println("Вы открыли 2 словарь, выберите команду из указаного списка");
+                    dictionary.Conclusion();
+                    menu2();
+                    break;
+                case "21":
+                    System.out.println("Введите ключ слова которое вы хотите удалить из 2 словаря");
+                    key = in.next();
+                    dictionary.Removal();
+                    dictionary.Conclusion();
+                    menu2();
+                    break;
+                case "22":
+                    System.out.println("Введите ключ слова которое вы хотите добавить во 2 словарь (Ключ - слово из 4 латинских букв)");
+                    wordLetters = sr.next();
+
+                    System.out.println("Введите само слово (значение - пятизначное число)");
+                    wordNumbers = sr.next();
+                    dictionary.ChecktheWord();
+                    dictionary.Conclusion();
+                    menu2();
+                    break;
+                case "23":
+                    System.out.println("Введите ключ слова которое вы хотите найти во 2 словаре.");
+                    keyWords = scanner.next();
+                    dictionary.Search();
+                    menu2();
+                    break;
+                default:
+                    System.out.println("Выбрана неверная команда. Будьте внимательней!\nВыберите команду ещё раз");
+                    break;
             }
-        } catch (Exception e) {
-            System.out.println("Введено некоретное значение");
         }
     }
     public static void menu1(){
